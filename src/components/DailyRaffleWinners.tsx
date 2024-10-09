8d8ac48566a8eedb4263228c5c2f267c90aaa2e6
McLovin <mclovinmohammed1981@gmail.com>
2024-10-09 16:59:02 +0200

    home page styling fix

--- src/components/DailyRaffleWinners.tsx
+++ src/components/DailyRaffleWinners.tsx
@@ -3,7 +3,7 @@ import RaffleWinnerCard from "@/components/RaffleWinnerCard.tsx";
 export default function DailyRaffleWinners(){
     return(
         <>
-            <h2 className='text-3xl text-white font-block mb-10'>Daily Raffle Winners!</h2>
+            <h2 className='text-3xl text-white font-block mb-10 text-center'>Daily Raffle Winners!</h2>
             <div className='flex justify-center gap-3 mb-20'>
                 <RaffleWinnerCard />
                 <RaffleWinnerCard />
