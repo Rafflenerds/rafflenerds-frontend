66dda9862299dbd900e7eb3749d351f990724645
ColourfulMelon <jordynnitch@gmail.com>
2024-11-13 12:19:06 +0200

    credits page responsive

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -24,25 +24,24 @@ export default function Credits() {
                 <h1 className="font-block text-2xl text-secondary mb-8">Credits</h1>
 
                 {/*box*/}
-                <div className="flex flex-col items-center justify-center border border-primary rounded-xl py-8 px-10 max-w-[45rem] w-full">
+                <div className="flex flex-col items-center justify-center border border-primary rounded-xl p-4 xl:py-8 xl:px-10 max-w-[45rem] w-full">
                     <div className='flex justify-end w-full'>
                         <Image src={iconCoin} alt='Coin Icon'/>
                         <p className='text-primary font-mono ml-2 text-2xl'>999</p>
                     </div>
 
                     {/*buy and redeem*/}
-                    <div className='flex justify-between w-full p-y2 mt-5'>
+                    <div className='flex flex-col gap-4 justify-between w-full p-y2 mt-5 xl:flex-row'>
                         <PrimaryButton active={true} name='Buy Credits'/>
                         <SecondaryButton name='Redeem Credits' className='py-0 text-primary'/>
                     </div>
 
                     {/*amount and currency*/}
                     <div className='flex justify-between border border-primary rounded-2xl w-full px-6 py-5 my-5'>
-                        {/*<p className='text-white font-block'>500</p>*/}
-                        <Input className='text-white font-block !border-none focus-visible:border-none focus-visible:ring-0 text-xl w-2/5' type="number" defaultValue={500} />
+                        <Input className='text-white font-block !border-none focus-visible:border-none focus-visible:ring-0 xl:text-xl w-2/5' type="number" defaultValue={500} />
 
                         <DropdownMenu>
-                            <DropdownMenuTrigger className='text-secondary font-block text-xs flex my-auto'>
+                            <DropdownMenuTrigger className='text-secondary font-block text-sm xl:text-base flex my-auto'>
                                 USDT
                                 <Image className='my-auto ml-2' src={iconDropdown} alt='Dropdown'/>
                             </DropdownMenuTrigger>
