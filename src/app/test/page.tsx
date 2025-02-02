d235996b76b22c62b189fb0e69202af22f08e0c3
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-02-02 12:06:46 +0200

    feat: Implement RaffleWinnerCard component

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,7 @@
-import RaffleWinnerTest from "@/components/raffleWinnerTest";
-
-
 export default function Raffle(){
     return (
         <div className="h-screen">
-            <RaffleWinnerTest />
-            
+
         </div>
     )
 }
